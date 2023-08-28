39353ac1bff28a2154b89abe8972daae585096db
MarinaNakhamchik <marinanakhamchik@gmail.com>
2023-08-28 17:20:53 +0300

    ls 16

---
+++ home_16/ls_17.py
@@ -0,0 +1,13 @@
+# a = (1 ,2, 3, 4, 5, 6, 7)
+# b = (2, 7, 8, 9, 5, 4, 0)
+# n = set(a)
+# c = frozenset(b)
+# print(tuple(n))
+# print(tuple(c))
+
+# r = n +c 
+# print(r)
+
+persen = {'name' : 'anna' , 'age' : 17 , 'city' : 'moscow'}
+
+print(persen['age'])
