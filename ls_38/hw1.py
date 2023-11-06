337dc0ea21c7f33e5352953e70fb66518e7b87c6
MarinaNakhamchik <marinanakhamchik@gmail.com>
2023-11-06 14:13:27 +0300

    hw 1/2

---
+++ ls_38/hw1.py
@@ -0,0 +1,28 @@
+def square(width, height, coordinate):
+    area = width * height
+    area_1 = 0
+
+    for i in coordinate:
+        x1, y1, x2, y2 = i
+        area_1 += (x2 - x1) * (y2 - y1)
+
+    area_2 = area - area_1
+    return area_2    
+
+
+width = int(input("Введите ширину матрицы: "))
+height = int(input("Введите высоту матрицы: "))
+rectangle = int(input("Введите количество прямоугольников: "))
+
+coordinate = []
+
+for i in range(rectangle):
+    pair = map(int, input("Введите координаты(x1, y1, x2, y2): ").split())
+    coordinate.append((pair))
+
+
+
+area_2 = square(width, height, coordinate)
+print(f"Незакрашенная площадь : {area_2}")
+
+
